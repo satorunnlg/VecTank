6a97558bf04ef5a3e93c686add738061883b41dd
satorunnlg <satorunnlg@gmail.com>
2025-04-15 08:19:57 +0900

    バージョン番号修正

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='VecTank',
-    version='0.1.0',
+    version='0.1.4',
     description='軽量で高速なインメモリベクトルデータベースライブラリ',
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
